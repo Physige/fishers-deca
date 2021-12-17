e7aa59aa7f8c0d6f7987d7fcc9e0072f34f884c3
physige <niaar2004@gmail.com>
2021-12-17 11:11:26 -0500

    quiz functionality

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,30 +1,50 @@
 import React, {useState} from "react"
 import { Questions } from "../Helpers/Questions"
 
-var currQuestionIndex = 0;
-var optionsChosen = [];
-
-// creates empty 2d array with sub arrays corresponding with each question
-for (var i = 0; i < Questions.length; i++) {optionsChosen.push([])}
-
 function Quiz() {
+
   // changes state based on current question
-  const [currQuestion, setCurrQuestion] = useState(currQuestionIndex);
+  const [currQuestion, setCurrQuestion] = useState(0);
+
+  // makes empty 2d array corresponding to each question
+  const [checkboxStates, setCheckboxStates] = useState(Questions.map(() => {return [];}));
 
   const previousQuestion = () => {
-    currQuestionIndex--;
-    setCurrQuestion(currQuestionIndex);
+    setCurrQuestion(currQuestion - 1);
   }
   
   const nextQuestion = () => {
-    currQuestionIndex++;
-
     // if finish button is clicked, submit form
-    if (currQuestionIndex >= Questions.length) {/*TODO*/}
+    if (currQuestion >= Questions.length) {/*TODO*/}
 
-    setCurrQuestion(currQuestionIndex);
+    setCurrQuestion(currQuestion + 1);
   }
 
+  // populates current question with previously selected answers or updates question with new selections
+  const populateCheckboxes = (e) => {
+    // current state of checkboxStates
+    let allQuestionsState = [... checkboxStates];
+
+    // current state of checkboxes on current question
+    let currQuestionState = [... checkboxStates[currQuestion]];
+
+    let checked = e.target.checked;
+    
+    if (checked) {
+      // adds id of selected question to corresponding array in checkboxStates
+      currQuestionState.push(e.target.id);
+      allQuestionsState[currQuestion] = currQuestionState;
+      console.log(allQuestionsState);
+      setCheckboxStates(allQuestionsState);
+    } else {
+      // removes id of selected question to corresponding array in checkboxStates
+      currQuestionState.splice(currQuestionState.indexOf(e.target.id), 1);
+      allQuestionsState[currQuestion] = currQuestionState;
+      console.log(allQuestionsState);
+      setCheckboxStates(allQuestionsState);
+    }
+  };
+  
   return (
     <div className="Quiz">
       <h1>{Questions[currQuestion].question}</h1>
@@ -36,43 +56,24 @@ function Quiz() {
           gets the values, exports values as array (Object.values()), 
           deletes the first key/value since thats the question/prompt(slice(1)),
           loops through array which also displays the values as buttons(map()) */
-          Object.values(Questions[currQuestion]).slice(1).map((option, i) => (
-            //<button onClick={() => onOptionUpdate(optionsChosen, i)}>{option}</button>
-            <form>
-              <label>
-                <input type="checkbox" id={i} onClick={(e) => {onOptionUpdate(e);}}></input>
-                {option}
-              </label>
-            </form>
-          ))
+          Object.values(Questions[currQuestion]).slice(1).map((option, i) => { //REFACTOR LATER <====================================
+            return (
+              <form>
+                <label>
+                  <input type="checkbox" id={i} onChange={(e) => populateCheckboxes(e)} 
+                    checked={checkboxStates[currQuestion].indexOf(String(i)) !== -1 ? true : false}></input>
+                  {option}
+                </label>
+              </form>
+            )
+          })
         }
+        
       </div>
-      <button disabled={currQuestionIndex <= 0 ? true : false} onClick={() => previousQuestion()}>Back</button>
-      <button onClick={() => nextQuestion()}>{currQuestionIndex >= Questions.length - 1 ? "Finish" : "Next"}</button>
+      <button onClick={() => previousQuestion()} disabled={currQuestion <= 0 ? true : false}>Back</button>
+      <button onClick={() => nextQuestion()}>{currQuestion >= Questions.length - 1 ? "Finish" : "Next"}</button>
     </div>
   );
 }
-
-// fills current question with previously selected answers
-function populateOptions() {
-  // TODO
-  console.log("finished")
-  //const currQuestionChosen
-}
-
-// updates option selection by adding the index of the selected option to optionsChosen array
-const onOptionUpdate = (e) => {
-  const currOptionsChosen = optionsChosen[currQuestionIndex];
-  const checked = e.target.checked;
-  if (checked) {
-    // adds index of option selected to the corresponding question array if checked
-    currOptionsChosen.push(e.target.id);
-    console.log(optionsChosen);
-  } else {
-    // removes index of option selected to the corresponding question array if unchecked
-    currOptionsChosen.splice(currOptionsChosen.indexOf(e.target.id), 1);
-    console.log(optionsChosen);
-  }
-};
 
 export default Quiz;
